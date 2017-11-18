a1c5338c3bad24587d2344801af644ba6120e856
willyb321 <wbwilliam7@gmail.com>
2017-11-18 17:35:12 +1100

    update description

--- src/About.js
+++ src/About.js
@@ -8,9 +8,13 @@ class About extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-4"><h2 className="heading">About Me</h2></div>
-						<div className="col-md-8"><p>Hi, I'm William, and I develop software. I am currently in my last year
+						<div className="col-md-8">
+							<p>
+								Hi, I'm William, and I develop software. I have currently just finished my last year
 							of High School (Year 12), studying Software Design and Development, Information Digital
-							Technology, Business Studies, plus the normal subjects (English, Maths).</p></div>
+							Technology, Business Studies, plus the normal subjects (English, Maths). I will be doing TAFE in 2018.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
