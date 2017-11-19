a955e52cd099855e52dec2a35adc0360cafa488b
willyb321 <wbwilliam7@gmail.com>
2017-11-19 13:31:29 +1100

    update skills stuff

--- src/Skills.js
+++ src/Skills.js
@@ -8,21 +8,24 @@ class Skills extends Component {
 			skills: [
 				{name: 'JavaScript', url: 'https://www.javascript.com/'},
 				{name: 'TypeScript', url: 'http://www.typescriptlang.org/'},
+				{name: 'HTML', url: 'https://en.wikipedia.org/wiki/HTML'},
+				{name: 'Pug', url: 'https://pugjs.org'},
 				{name: 'Node.JS', url: 'https://nodejs.org'},
-				{name: 'Python', url: 'https://www.python.org/'},
-				{name: 'Golang', url: 'https://golang.org/'},
-				{name: 'C#', url: 'https://en.wikipedia.org/wiki/C_Sharp_%28programming_language%29'},
+				{name: 'MeteorJS', url: 'https://meteor.com'},
 				{name: 'Electron', url: 'https://electron.atom.io'},
 				{name: 'VueJS', url: 'https://vuejs.org/'},
 				{name: 'React', url: 'https://reactjs.org/'},
-				{name: 'Sass', url: 'http://sass-lang.com/'},
-				{name: 'SCSS', url: 'http://sass-lang.com/'},
+				{name: 'Sass / SCSS', url: 'http://sass-lang.com/'},
 				{name: 'CSS', url: 'https://en.wikipedia.org/wiki/Cascading_Style_Sheets'},
-				{name: 'MeteorJS', url: 'https://meteor.com'},
-				{name: 'PouchDB', url: 'https://pouchdb.com/'},
-				{name: 'MongoDB', url: 'https://www.mongodb.com/'},
 				{name: 'Mongoose', url: 'http://mongoosejs.com/'},
-				{name: 'NeDB', url: 'https://github.com/louischatriot/nedb/'}
+				{name: 'Python', url: 'https://www.python.org/'},
+				{name: 'Ruby', url: 'https://www.ruby-lang.org/en/'},
+				{name: 'Golang', url: 'https://golang.org/'},
+				{name: 'C#', url: 'https://en.wikipedia.org/wiki/C_Sharp_%28programming_language%29'},
+				{name: 'Bash', url: 'https://en.wikipedia.org/wiki/Bash_(Unix_shell)'},
+				{name: 'ZSH', url: 'https://en.wikipedia.org/wiki/Z_shell'},
+				{name: 'PouchDB', url: 'https://pouchdb.com/'},
+				{name: 'MongoDB', url: 'https://www.mongodb.com/'}
 			]
 		};
 	}
