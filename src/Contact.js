6280ba722a0423454d4a89eddb050115e3069d93
willyb321 <wbwilliam7@gmail.com>
2017-11-20 18:14:15 +1100

    Revert "fancy spinner to submit form"
    This reverts commit 12082a829c604d7d03cf455d98fa02428d029786.

--- src/Contact.js
+++ src/Contact.js
@@ -1,37 +1,12 @@
 import React, {Component} from 'react';
 import './main.css';
-import * as r2 from 'r2';
 
 class Contact extends Component {
-	constructor() {
-		super();
-
-		this.state = {
-			submitting: 'spinner hidden'
-		};
-	}
-	async submitForm(e) {
-		e.preventDefault();
-		e.persist();
-		const data = {message: e.target[2].value, _replyto: e.target[1].value};
-		if (!data._replyto || !data.message) {
-			return;
-		}
-		e.target[3].disabled = true;
-		await r2.post('https://formspree.io/wbwilliam7@gmail.com', {json: data, Accept: 'application/json'}).text;
-		return Promise.resolve();
-	}
 	render() {
 		return (
 			<div id="contact"><h2>Get in Touch</h2>
 				<div id="contact-form">
-					<form onSubmit={e => {
-						this.setState({submitting: 'spinner'});
-						this.submitForm(e)
-						.then(() => {
-							this.setState({submitting: 'spinner hidden'});
-						});
-					}}>
+					<form action="https://formspree.io/wbwilliam7@gmail.com" method="POST">
 						<input name="_subject" type="hidden"
 							   value="Contact request from personal website"></input><input
 						name="_replyto" placeholder="Your email" required="" type="email"></input>
@@ -40,13 +15,6 @@ class Contact extends Component {
 								  required=""></textarea>
 						<button type="submit">Send</button>
 					</form>
-					<div className={this.state.submitting}>
-						<div className="rect1"></div>
-						<div className="rect2"></div>
-						<div className="rect3"></div>
-						<div className="rect4"></div>
-						<div className="rect5"></div>
-					</div>
 				</div>
 			</div>
 		);
