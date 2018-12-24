d8e3b93819821c1d5de37cbd46ff0c461eac350c
William Blythe <wbwilliam7@gmail.com>
2018-12-25 08:22:36 +1100

    fix netlify form

--- src/Contact.js
+++ src/Contact.js
@@ -6,9 +6,10 @@ class Contact extends Component {
 		return (
 			<div id="contact"><h2>Get in Touch</h2>
 				<div id="contact-form">
-					<form name="Contact" method="POST" data-netlify="true">
-						<input name="_subject" type="hidden"
-							   value="Contact request from personal website"/>
+					<form name="contact" method="POST" data-netlify="true">
+						<input type="hidden" name="form-name" value="contact"/>
+						<input
+							name="name" placeholder="Your name" required="true" type="text"/>
 						<input
 							name="email" placeholder="Your email" required="true" type="email"/>
 						<textarea name="message"
